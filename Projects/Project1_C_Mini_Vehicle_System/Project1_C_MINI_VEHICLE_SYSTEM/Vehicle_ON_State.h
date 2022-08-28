66a9a015bc58dda880bf5858d52f87e10ee850d4
youssefnashaatt <112262361+youssefnashaatt@users.noreply.github.com>
2022-08-28 22:56:20 +0200

    Add files via upload

---
+++ Projects/Project1_C_Mini_Vehicle_System/Project1_C_MINI_VEHICLE_SYSTEM/Vehicle_ON_State.h
@@ -0,0 +1,17 @@
+#include "Defined_Types.h"
+
+#ifndef VEHICLE_ON_H_
+#define VEHICLE_ON_H_
+
+
+void Vehicle_ON(void);
+char Vehicle_ON_Menu(void);
+void CASE_B_Vehicle_ON(void);
+void CASE_C_Vehicle_ON(void);
+void CASE_D_Vehicle_ON(void);
+void Vehicle_speed_30(void);
+void Display_Car_State(void);
+char * check_ON_OFF(bool v);
+
+
+#endif /* VEHICLE_ON_H_ */
---
+++ Projects/Project1_C_Mini_Vehicle_System/Project1_C_MINI_VEHICLE_SYSTEM/Vehicle_State_Control.h
@@ -0,0 +1,9 @@
+#ifndef VEHICLE_STATE_CONTROL_H_
+#define VEHICLE_STATE_CONTROL_H_
+
+
+void Vehicle_Control(void);
+char Vehicle_State(void);
+
+
+#endif /* VEHICLE_STATE_CONTROL_H_ */
