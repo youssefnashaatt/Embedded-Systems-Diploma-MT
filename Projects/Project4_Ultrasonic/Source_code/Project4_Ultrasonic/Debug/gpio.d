4b81a519cf5f2030f752de2cf46d43f4fa56e586
youssefnashaatt <112262361+youssefnashaatt@users.noreply.github.com>
2023-01-04 22:57:25 +0200

    Add files via upload

---
+++ Projects/Project4_Ultrasonic/Source_code/Project4_Ultrasonic/Debug/gpio.d
@@ -0,0 +1,7 @@
+gpio.o gpio.o: ../gpio.c ../gpio.h ../Standard_Types.h ../MACROS.h
+
+../gpio.h:
+
+../Standard_Types.h:
+
+../MACROS.h:
---
+++ Projects/Project4_Ultrasonic/Source_code/Project4_Ultrasonic/Debug/subdir.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../ICU.c \
+../LCD.c \
+../Main.c \
+../Ultrasonic.c \
+../gpio.c 
+
+OBJS += \
+./ICU.o \
+./LCD.o \
+./Main.o \
+./Ultrasonic.o \
+./gpio.o 
+
+C_DEPS += \
+./ICU.d \
+./LCD.d \
+./Main.d \
+./Ultrasonic.d \
+./gpio.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR Compiler'
+	avr-gcc -Wall -g2 -gstabs -O0 -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega32 -DF_CPU=8000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
